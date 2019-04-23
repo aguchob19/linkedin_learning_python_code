7fde20f0c8786156d611a04dc231d5aafc018a68
aguch <agucho_b19@hotmail.com>
2019-04-23 18:20:09 +0200

    first commit hellow world variables

---
+++ ch2/variables.py
@@ -0,0 +1,22 @@
+# Declare a variable and initialize it
+f = 0
+# print(f)
+
+# re-declaring the variable works
+# f = "abc"
+# print(f)
+
+# ERROR: variables of different types cannot be combined
+# print("this is a string" + str(123))
+
+# Global vs. local variables in functions
+def someFuntion():
+    global f
+    f = "def"
+    print(f)
+
+someFuntion()
+print(f)
+
+del f
+print(f)
